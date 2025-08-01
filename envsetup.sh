c6c6a52fec51834ef8ec90c9fddb21bd09d98269
Kun-Yi Chen <kunyi.chen@gmail.com>
2025-08-01 20:50:14 +0800

    fix alias push-rkipc change directory issues

--- envsetup.sh
+++ envsetup.sh
@@ -2,7 +2,9 @@
 
 PATH=${PWD}/arm-rockchip830-linux-uclibcgnueabihf/bin:$PATH
 
-alias push-rkipc='adb push ${PWD}/build/src/rv1106_ipc/rkipc /oem/usr/bin/rkipc'
+TOP=$(realpath "$(dirname "${BASH_SOURCE[0]}")")
+
+alias push-rkipc='adb push ${TOP}/build/src/rv1106_ipc/rkipc /oem/usr/bin/rkipc'
 alias stop-RkLunch='adb shell RkLunch-stop.sh'
 alias stop-rkipc='adb shell killall -9 rkipc'
 alias start-rkipc='adb shell rkipc -a /oem/usr/share/iqfiles &'
